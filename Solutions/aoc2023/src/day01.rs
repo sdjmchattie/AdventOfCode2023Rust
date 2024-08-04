08488fd7e10ffab6f7c749b51af649eeee23f5a6
Stuart McHattie <stuart.mchattie@gmail.com>
2024-08-04 19:38:34 +0100

    Try using a HashMap for day 1

--- Solutions/aoc2023/src/day01.rs
+++ Solutions/aoc2023/src/day01.rs
@@ -1,88 +1,38 @@
-struct NumMatch<'a> {
-    num: &'a str,
-    val: u32,
-}
+use hashbrown::HashMap;
 
-fn sum_first_last_digits(input: &str, matchers: Vec<NumMatch>) -> String {
+type Matchers = HashMap<String, u32>;
+
+fn sum_first_last_digits(input: &str, matchers: &Matchers) -> String {
     let sum = input.lines().fold(0, |sum, line| {
         let mut left: Option<u32> = None;
-        let mut right: Option<u32> = None;
+        let mut right: u32 = 0;
 
         for (i, _) in line.chars().enumerate() {
-            for m in matchers.iter() {
-                if line[i..].starts_with(m.num) {
-                    left = left.or(Some(m.val));
-                    right = Some(m.val);
+            for (num, &val) in matchers.iter() {
+                if line[i..].starts_with(num) {
+                    left = left.or(Some(val));
+                    right = val.to_owned();
                 }
             }
         }
 
-        return sum + left.unwrap() * 10 + right.unwrap();
+        return sum + left.unwrap() * 10 + right;
     });
 
     return sum.to_string();
 }
 
-fn part_1(input: &str) -> String {
-    let matchers = vec![
-        NumMatch { num: "1", val: 1 },
-        NumMatch { num: "2", val: 2 },
-        NumMatch { num: "3", val: 3 },
-        NumMatch { num: "4", val: 4 },
-        NumMatch { num: "5", val: 5 },
-        NumMatch { num: "6", val: 6 },
-        NumMatch { num: "7", val: 7 },
-        NumMatch { num: "8", val: 8 },
-        NumMatch { num: "9", val: 9 },
-    ];
-
-    return sum_first_last_digits(input, matchers);
-}
-
-fn part_2(input: &str) -> String {
-    let matchers = vec![
-        NumMatch { num: "1", val: 1 },
-        NumMatch { num: "2", val: 2 },
-        NumMatch { num: "3", val: 3 },
-        NumMatch { num: "4", val: 4 },
-        NumMatch { num: "5", val: 5 },
-        NumMatch { num: "6", val: 6 },
-        NumMatch { num: "7", val: 7 },
-        NumMatch { num: "8", val: 8 },
-        NumMatch { num: "9", val: 9 },
-        NumMatch { num: "one", val: 1 },
-        NumMatch { num: "two", val: 2 },
-        NumMatch {
-            num: "three",
-            val: 3,
-        },
-        NumMatch {
-            num: "four",
-            val: 4,
-        },
-        NumMatch {
-            num: "five",
-            val: 5,
-        },
-        NumMatch { num: "six", val: 6 },
-        NumMatch {
-            num: "seven",
-            val: 7,
-        },
-        NumMatch {
-            num: "eight",
-            val: 8,
-        },
-        NumMatch {
-            num: "nine",
-            val: 9,
-        },
-    ];
-
-    return sum_first_last_digits(input, matchers);
-}
-
 pub fn run(input: &str) {
-    println!("Part 1 solution: {}", part_1(input));
-    println!("Part 2 solution: {}", part_2(input));
+    let digit_matchers: Matchers = (0..=9).map(|i| (i.to_string(), i)).collect();
+    let part_1 = sum_first_last_digits(input, &digit_matchers);
+    println!("Part 1 solution: {}", part_1);
+
+    let part2_matchers: Matchers = "one,two,three,four,five,six,seven,eight,nine"
+        .split(',')
+        .map(String::from)
+        .zip(1..=9)
+        .chain(digit_matchers)
+        .collect();
+    let part_2 = sum_first_last_digits(input, &part2_matchers);
+    println!("Part 2 solution: {}", part_2);
 }
